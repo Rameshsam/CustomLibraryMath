9548a470abc0aaad4248c79b2b4074fec514c77b
Ramesh Sambu <rameshsambu44@gmail.com>
2018-07-19 12:20:38 +0530

    Initial release
    Initial release

---
+++ custommath/src/main/java/colortoast/com/custommath/LibMath.java
@@ -0,0 +1,28 @@
+package colortoast.com.custommath;
+
+public class LibMath {
+
+    public static int add(int a,int b)
+    {
+        return a+b;
+    }
+
+
+    public static int subtract(int a,int b)
+    {
+        return a-b;
+    }
+
+
+    public static int multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+
+    public static int devide(int a,int b)
+    {
+        return a%b;
+    }
+
+}
